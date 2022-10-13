b45cee7a3c9f5b109b62e5a73aa613a64d4acfce
alexbs01 <alex.becerra.suarez01@gmail.com>
2022-10-13 17:52:30 +0200

    Error con el número de iteraciones del bucle resuelto
    La medición de tiempos se separó en dos bucle do-while distintos
    para en uno hacer todas las mediciones relacionadas con el
    algoritmo de ordenación por inserción, y en el otro con
    Quicksort

--- ordenInsercYOrdenRapida/main.c
+++ ordenInsercYOrdenRapida/main.c
@@ -11,40 +11,65 @@
 
 int main() {
     inicializar_semilla(); // Inicializamos la seed para obtener números aleatorios
-    int numeroElementos = 100;
+    int numeroElementosInicial = 1, numeroElementos = numeroElementosInicial, elementosTotales = 1000;
 
+    // Inicializamos el array con el número de elementos que tendrá al final de la última prueba
+    int arrayNumeros[elementosTotales];
 
-    //printf("Escribe cuantos elementos quieres en el array: ");
-    //scanf("%d", &numeroElementos);
-    int arrayNumeros[numeroElementos];
-
+    // Medición de tiempos con el algoritmo de ORDENACIÓN POR INSERCIÓN
+    printf("Medición de tiempos con ordenación por inserción");
     do {
         printf("\n\nTiempos para un array de %d elementos\n", numeroElementos);
+
         // Calculamos los tiempos con ambos algoritmos con un array de números aleatorios
         printf("Numeros aleatorios\n");
         aleatorio(arrayNumeros, numeroElementos);
         tiempoOrdenacionInsercion(arrayNumeros, numeroElementos);
+
+        // Calculamos los tiempos utilizando un array ya ordenado de menor a mayor
+        printf("\nNumeros ascendentes\n");
+        ascendente(arrayNumeros, numeroElementos);
+        tiempoOrdenacionInsercion(arrayNumeros, numeroElementos);
+
+        // Volvemos a calcular los tiempos, pero esta vez con un array ordenado de mayor a menor
+        printf("\nNumeros descendentes\n");
+        descendente(arrayNumeros, numeroElementos);
+        tiempoOrdenacionInsercion(arrayNumeros, numeroElementos);
+
+        printf("\n***************************************************");
+
+        numeroElementos *= 10;
+
+    } while(numeroElementos <= elementosTotales);
+
+    numeroElementos = numeroElementosInicial; // Restablecemos el número de elementos
+
+    // Medición de tiempos con QUICKSORT
+    printf("\n***************************************************");
+    printf("\n\nMedición de tiempos con Quicksort\n");
+
+    do {
+        printf("\nTiempos para un array de %d elementos\n", numeroElementos);
+
+        // Calculamos los tiempos con ambos algoritmos con un array de números aleatorios
+        printf("Numeros aleatorios\n");
         aleatorio(arrayNumeros, numeroElementos);
         tiempoOrdenacionQuicksort(arrayNumeros, numeroElementos);
 
         // Calculamos los tiempos utilizando un array ya ordenado de menor a mayor
         printf("\nNumeros ascendentes\n");
         ascendente(arrayNumeros, numeroElementos);
-        tiempoOrdenacionInsercion(arrayNumeros, numeroElementos);
-        ascendente(arrayNumeros, numeroElementos);
         tiempoOrdenacionQuicksort(arrayNumeros, numeroElementos);
 
         // Volvemos a calcular los tiempos, pero esta vez con un array ordenado de mayor a menor
         printf("\nNumeros descendentes\n");
         descendente(arrayNumeros, numeroElementos);
-        tiempoOrdenacionInsercion(arrayNumeros, numeroElementos);
-        descendente(arrayNumeros, numeroElementos);
         tiempoOrdenacionQuicksort(arrayNumeros, numeroElementos);
 
-        printf("\n***************************************************");
+        printf("\n***************************************************\n");
 
         numeroElementos *= 10;
-    } while(numeroElementos <= 100000);
+    } while(numeroElementos <= elementosTotales);
 
 
     return 0;
